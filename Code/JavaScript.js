db1dbc4552785d7d58158153e5246f4a2b5c6ebb
Michal Topinka <1602010585@sz.pku.edu.cn>
2017-12-08 21:57:40 +0100

    added Spotify API code + calendar crossing

--- Code/JavaScript.js
+++ Code/JavaScript.js
@@ -1,48 +1,93 @@
  $(document).ready(function(){
- 	
-<<<<<<< HEAD
- 		
+ 
+ 	// Michal
+
+ 	// SPOTIFY
+	function showURLs(response){
+		console.log(response)
+		var col = $(document.createElement('div'));
+		col.attr('class','col-6');
+		response.playlists.items.forEach(function(item){
+			var row = $(document.createElement('div'));
+			row.attr('class','row');
+			var image = $(document.createElement('img'));
+			image.attr('src',item.images[0].url);
+			image.attr('alt','playlist image')
+			image.attr('height',50);
+			image.appendTo(row);
+			row.append('<span><a href = "#APIs" data-playlist-uri="' + item.uri + '"">' + item.name + '</a></span>');
+			row.appendTo(col);
+		});
+		col.appendTo('.playlists');
+		var col_2 = $(document.createElement('div'));
+		col_2.attr('class','col-6 spotifyPlayer');
+		col_2.appendTo('.playlists');
+	};
+
+	function retrievePlaylists(searchQuery){
+		$.ajax({
+			url: 'https://api.spotify.com/v1/search',
+			method: 'GET',
+			accepts: 'application/json',
+			headers: {
+				Authorization: 'Bearer BQBvvxWkhcvi25bCIhJSDEvdM-MIBxzKmJnwHLG_dh8lCWpFkzlgF77j0BKxn1WIh3c-ZZuUeIMtyZYk-LKiqhl--f2ANPvxzGcCMB7QmVvxlFaifeNRQ6Yv_ji5qDS1Bbe-wl5lyWP2vip3Hns'
+				// Note: the authorization token expires after one hour!
+				// It cannot be done in other way unless I put my Client Secret ID in the code.
+				// Therefore, before the website is presented new token has to be generated.
+			},
+			data: {
+				q: searchQuery,
+				type: 'playlist',
+				limit: 10
+			},
+			// success: function(response){console.log(response)}
+			success: showURLs
+	});
+	}
+
+	$('button').click(function(e){
+		e.preventDefault();
+		$('.playlists').empty();
+		searchQuery = $('.search:input').val();
+		retrievePlaylists(searchQuery);
+	});
+
+	// $('input').keydown(function(e){
+	// 	if (e.keyCode == 13){
+	// 		$('.playlists').empty();
+	// 		searchQuery = $('.search:input').val();
+	// 		retrievePlaylists(searchQuery);
+	// 	};
+	// });
+
+	$('body').on('click','a',function(){
+		var uri = $(this).attr('data-playlist-uri');
+		$('.spotifyPlayer').empty();
+		$('.spotifyPlayer').append('<iframe src="https://open.spotify.com/embed?uri=' + uri + '&theme=white" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>');
+	});
+
+	// CALENDAR
+	for (i=1; i<=8; i++){
+		$('.firstRow').append('<div id="' + i + '" class="col cal"><span>' + i + '</span></div>');
+	};
+
+	for (i=9; i<=16; i++){
+		$('.secondRow').append('<div id="' + i + '" class="col cal"><span>' + i + '</span></div>');
+	};
+
+	for (i=17; i<=24; i++){
+		$('.thirdRow').append('<div id="' + i + '" class="col cal"><span>' + i + '</span></div>');
+	};
+
+
+    for (i=1; i<=24; i++){
+		var d = new Date();
+	    var n = d.getDate();
+    	if($('#' + i).attr('id')<n){
+    		$('#' + i).addClass('black');
+    	};
+    };
+
+ 	// Michal (end)
+
  });
-
-<<<<<<< HEAD
-=======
-
-<<<<<<< HEAD
-=======
-	// 2. This code loads the IFrame Player API code asynchronously.
- 	var tag = document.createElement('script');
-    tag.src = "https://www.youtube.com/iframe_api";
-    var firstScriptTag = document.getElementsById('script')[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
->>>>>>> origin/master
-=======
- 	
- 		console.log('hi');
->>>>>>> 008d3393193ed146c7df3ab667fdb49e3b72293e
-
- 	$.ajax({
- 		url: 'https://api.spotify.com/v1/search',
- 		method: 'GET',
- 		contentType: "application/json",
- 		Authorization: 'Bearer BQAJ4LKIb234-X7GZJR_96qC-edTPzgFMqkoGM9TiIedPiCcU8iX_s3An9-KhmkdNzaMfvIm8BWRmOawqUnVjGfDfqDbHGqf30uDjwc0tUJ0S2_8F3ZysgOCysoRSYrbLamNiGoluCz2ON_6uQo',
- 		data: {
- 			q: 'christmas',
- 			type: 'playlist',
- 			limit: 5
- 		},
- 		success: displayResults
-    });
- 	
-function displayResults(response_body){
-	console.log(response_body);
-}
-
- });
-<<<<<<< HEAD
->>>>>>> 37cc02a2a0b72d43f42b8f32d5704860da3f7856
-
-
-
-=======
->>>>>>> 008d3393193ed146c7df3ab667fdb49e3b72293e
-
