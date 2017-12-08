008d3393193ed146c7df3ab667fdb49e3b72293e
Michal Topinka <1602010585@sz.pku.edu.cn>
2017-12-08 14:24:27 +0100

    commented gmaps iframe + changes in spotify api

--- Code/JavaScript.js
+++ Code/JavaScript.js
@@ -1,50 +1,24 @@
  $(document).ready(function(){
- 	$('.parallax-window').parallax();
- 		
- });
-
-<<<<<<< HEAD
-=======
-
-<<<<<<< HEAD
-=======
-	// 2. This code loads the IFrame Player API code asynchronously.
- 	var tag = document.createElement('script');
-    tag.src = "https://www.youtube.com/iframe_api";
-    var firstScriptTag = document.getElementsById('script')[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
->>>>>>> origin/master
+ 	
+ 	
+ 		console.log('hi');
 
  	$.ajax({
  		url: 'https://api.spotify.com/v1/search',
  		method: 'GET',
- 		contentType: "application/json; charset=UTF-8",
- 		Authorization: 'BQAJ4LKIb234-X7GZJR_96qC-edTPzgFMqkoGM9TiIedPiCcU8iX_s3An9-KhmkdNzaMfvIm8BWRmOawqUnVjGfDfqDbHGqf30uDjwc0tUJ0S2_8F3ZysgOCysoRSYrbLamNiGoluCz2ON_6uQo',
+ 		contentType: "application/json",
+ 		Authorization: 'Bearer BQAJ4LKIb234-X7GZJR_96qC-edTPzgFMqkoGM9TiIedPiCcU8iX_s3An9-KhmkdNzaMfvIm8BWRmOawqUnVjGfDfqDbHGqf30uDjwc0tUJ0S2_8F3ZysgOCysoRSYrbLamNiGoluCz2ON_6uQo',
  		data: {
  			q: 'christmas',
- 			type: 'playlist'
+ 			type: 'playlist',
+ 			limit: 5
  		},
- 		success: function (response) {
-            callback(response);
-        }
-
-
-
- 	});
-
- 	var searchAlbums = function (query) {
-    $.ajax({
-        url: 'https://api.spotify.com/v1/search',
-        data: {
-            q: query,
-            type: 'album'
-        },
-        success: function (response) {
-            resultsPlaceholder.innerHTML = template(response);
-        }
+ 		success: displayResults
     });
-};
-searchAlbums('let go');
  	
+function displayResults(response_body){
+	console.log(response_body);
+}
+
  });
->>>>>>> 37cc02a2a0b72d43f42b8f32d5704860da3f7856
+
