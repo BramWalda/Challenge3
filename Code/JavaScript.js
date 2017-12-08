b546db996e339d4b7b3d159e0a689664bbcb117c
Michal Topinka <1602010585@sz.pku.edu.cn>
2017-12-08 14:05:04 +0100

    changed jquery CDN

--- Code/JavaScript.js
+++ Code/JavaScript.js
@@ -1,41 +1,35 @@
  $(document).ready(function(){
 
-	// // 2. This code loads the IFrame Player API code asynchronously.
- // 	var tag = document.createElement('script');
- //    tag.src = "https://www.youtube.com/iframe_api";
- //    var firstScriptTag = document.getElementsById('script')[0];
- //    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-	// // 3. This function creates an <iframe> (and YouTube player) after the API code downloads.
- //    var player;
- //    function onYouTubeIframeAPIReady() {
- //      	player = new YT.Player('yt player', {
- //        		events: {
- //            		'onReady': onPlayerReady,
- //            		'onStateChange': onPlayerStateChange
- //          		}
- //        	});
- //      }
+ 	$.ajax({
+ 		url: 'https://api.spotify.com/v1/search',
+ 		method: 'GET',
+ 		contentType: "application/json; charset=UTF-8",
+ 		Authorization: 'BQAJ4LKIb234-X7GZJR_96qC-edTPzgFMqkoGM9TiIedPiCcU8iX_s3An9-KhmkdNzaMfvIm8BWRmOawqUnVjGfDfqDbHGqf30uDjwc0tUJ0S2_8F3ZysgOCysoRSYrbLamNiGoluCz2ON_6uQo',
+ 		data: {
+ 			q: 'christmas',
+ 			type: 'playlist'
+ 		},
+ 		success: function (response) {
+            callback(response);
+        }
 
-	// // 4. The API will call this function when the video player is ready.
- //    function onPlayerReady(event) {
- //    	event.target.playVideo();
- //    }
 
- //      // 5. The API calls this function when the player's state changes.
- //      //    The function indicates that when playing a video (state=1),
- //      //    the player should play for six seconds and then stop.
- //      // var done = false;
- //      // function onPlayerStateChange(event) {
- //      //   if (event.data == YT.PlayerState.PLAYING && !done) {
- //      //     setTimeout(stopVideo, 6000);
- //      //     done = true;
- //      //   }
- //      // }
- //      // function stopVideo() {
- //      //   player.stopVideo();
- //      // }
 
- 	
+ 	});
+
+ 	var searchAlbums = function (query) {
+    $.ajax({
+        url: 'https://api.spotify.com/v1/search',
+        data: {
+            q: query,
+            type: 'album'
+        },
+        success: function (response) {
+            resultsPlaceholder.innerHTML = template(response);
+        }
+    });
+};
+searchAlbums('let go');
  	
  });
